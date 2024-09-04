fe5be31883acc7f655672a863dc0f1b9b92569f1
vishal soni <vishal7898516588@gmail.com>
2024-09-04 20:37:51 +0530

    new

--- MajorProject/blogproject/src/App.jsx
+++ MajorProject/blogproject/src/App.jsx
@@ -1,14 +1,37 @@
-
+import { useEffect } from 'react'
+import { useState}  from 'react'
 import './App.css'
-
+import {useDispatch} from 'react-redux'
+import authService from './auth_services/auth'
+import {login,logout} from './store/authSlice'
+import {Header}  from './components/index'
+import {Footer} from './components/index'
 function App() {
-console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading,setLoading]=useState(true)
+  const dispatch=useDispatch()
+  useEffect(()=>{
+      authService.getCurrentUser()
+      .then((userData)=>{
+        if(userData){
+          dispatch(login({userData}))
+        }else{
+          dispatch(logout())
+        }
+       })
+      .finally(()=>setLoading(false))
+  },[])
 
-  return (
-    <>
-    <h1>vishaal</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-500'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          TODO:{/* outlet */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ):null
 }
 
 export default App
